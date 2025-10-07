5734ca5675676d7c7ceaceadfe2dcea94ea6ca51
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2025-10-07 19:33:33 +0300

    edite GlassBlock

--- hmelMap/src/components/GlassBlock/GlassBlock.tsx
+++ hmelMap/src/components/GlassBlock/GlassBlock.tsx
@@ -6,8 +6,9 @@ type GlassBlockProps = {
 
 export default function GlassBlock({title, text, size='sm'} : GlassBlockProps) {
     return (
-
-        <div className={`relative h-fit max-w-[${size=='lg' ? '1000px' : '340px'}]  rounded-2xl shadow-[0_0_4px_rgba(0,0,0,0.3)] backdrop-blur-sm bg-[rgba(255,255,255,0.3)] overflow-hidden border-[1px] border-[rgba(255,255,255,0.4)]`}>
+        <div
+            style={size === 'sm' ? {maxWidth: '340px'} : {maxWidth: '1000px'}}
+            className={`relative h-fit  rounded-2xl shadow-[0_0_4px_rgba(0,0,0,0.3)] backdrop-blur-sm bg-[rgba(255,255,255,0.3)] overflow-hidden border-[1px] border-[rgba(255,255,255,0.4)]`}>
             <div className='bg-green-600 relative top- left-0 right-0 px-2 py-1.5 rounded-tl-2xl rounded-br-2xl w-fit'>
                 <h5 className='m-0 text-white text-2xl'>{title}</h5>
             </div>
